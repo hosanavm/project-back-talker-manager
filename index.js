c3ec00b08fab20b396706901c5c832b55e66e783
root <hosanavms@gmail.com>
2022-08-03 21:47:32 +0000

    cria as validacoes para o endpoint /login

--- index.js
+++ index.js
@@ -7,11 +7,35 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_ALERT_STATUS = 400;
 const PORT = '3000';
 
 const readFile = () => fs.readFileSync('./talker.json', 'utf-8');
 
-// console.log(readRes[0].id);
+const valiEmail = (req, res, next) => {
+  const { email } = req.body;
+  const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!email) {
+    return res.status(HTTP_ALERT_STATUS).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!regexEmail.test(email)) {
+    return res.status(HTTP_ALERT_STATUS)
+    .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  next();
+};
+
+const valiPass = (req, res, next) => {
+  const { password } = req.body;
+  if (!password) {
+    return res.status(HTTP_ALERT_STATUS).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(HTTP_ALERT_STATUS)
+    .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  next();
+};
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -34,7 +58,7 @@ app.get('/talker/:id', (req, res) => {
   return res.status(HTTP_OK_STATUS).json(talkerId);
 });
 
-app.post('/login', (req, res) => {
+app.post('/login', valiEmail, valiPass, (_req, res) => {
   const token = crypto.randomBytes(8).toString('hex');
   res.status(HTTP_OK_STATUS).json({ token });
 });
