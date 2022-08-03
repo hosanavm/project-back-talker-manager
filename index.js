cf3a2e2d3192456866f0e2bd758f0563122034e2
Hosana <hosanavms@gmail.com>
2022-08-03 15:59:22 -0300

    cria o endpoint GET/talker

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,10 +8,19 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const readFile = () => fs.readFileSync('./talker.json', 'utf-8');
+
+function talkers(_req, res) {
+  const readRes = JSON.parse(readFile());
+  return res.status(200).json(readRes);
+}
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.get('/talker', talkers);
 
 app.listen(PORT, () => {
   console.log('Online');
