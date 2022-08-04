dcfbbf90b2b3cb72da73e20384b67d9bdf3686c3
root <hosanavms@gmail.com>
2022-08-04 18:31:50 +0000

    cria o endpoint POST/talker

--- index.js
+++ index.js
@@ -37,6 +37,74 @@ const valiPass = (req, res, next) => {
   next();
 };
 
+const valiTok = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+  if (authorization.length !== 16) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+  next();
+};
+
+const valiName = (req, res, next) => {
+  const { name } = req.body;
+  if (!name) {
+    return res.status(HTTP_ALERT_STATUS).json({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length < 3) {
+    return res.status(HTTP_ALERT_STATUS)
+    .json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  next();
+};
+
+const valiAge = (req, res, next) => {
+  const { age } = req.body;
+  if (!age) {
+    return res.status(HTTP_ALERT_STATUS).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (age < 18) {
+    return res.status(HTTP_ALERT_STATUS)
+    .json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  next();
+};
+
+const valiTalk = (req, res, next) => {
+  const { talk } = req.body;
+  if (!talk) {
+    return res.status(HTTP_ALERT_STATUS).json({ message: 'O campo "talk" é obrigatório' });
+  }
+  next();
+};
+
+const valiWatc = (req, res, next) => {
+  const { talk: { watchedAt } } = req.body;
+  const dateRegex = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+  if (!watchedAt) {
+    return res.status(HTTP_ALERT_STATUS).json({ message: 'O campo "watchedAt" é obrigatório' });
+  }
+  if (!dateRegex.test(watchedAt)) {
+    return res.status(HTTP_ALERT_STATUS)
+    .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  next();
+};
+
+const valiRate = (req, res, next) => {
+  const { talk: { rate } } = req.body;
+  if (!rate) {
+    return res.status(HTTP_ALERT_STATUS).json({ message: 'O campo "rate" é obrigatório' });
+  }
+  if (rate < 1 || rate > 5) {
+    return res.status(HTTP_ALERT_STATUS)
+    .json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  next();
+};
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -61,6 +129,25 @@ app.get('/talker/:id', (req, res) => {
 app.post('/login', valiEmail, valiPass, (_req, res) => {
   const token = crypto.randomBytes(8).toString('hex');
   res.status(HTTP_OK_STATUS).json({ token });
+});
+
+// console.log(talkers);
+app.post('/talker', valiTok, valiName, valiAge, valiTalk, valiWatc, valiRate, (req, res) => {
+  const { name, age, talk } = req.body;
+  const { watchedAt, rate } = talk;
+  const talkers = JSON.parse(readFile());
+  const talker = {
+    id: talkers.length + 1,
+    name,
+    age,
+    talk: {
+      watchedAt,
+      rate,
+    },
+  };
+  talkers.push(talker);
+  fs.writeFileSync('talker.json', JSON.stringify(talkers));
+  res.status(201).json(talker);
 });
 
 app.listen(PORT, () => {
